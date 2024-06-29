284ec51812f170e37eefe5305162fa5542a69309
Stefan Flyagin <ila723483@gmail.com>
2024-06-29 15:52:14 +0300

    Add project files.

---
+++ HomeWorkRepo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWorkRepo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
