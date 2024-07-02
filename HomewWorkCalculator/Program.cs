95f4b0877dd6d92748b5e2685c5c770518951313
Stefan Flyagin <ila723483@gmail.com>
2024-07-02 19:40:33 +0300

    Calculator

---
+++ HomewWorkCalculator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+
+namespace HomewWorkCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool buttonExit = true;
+            decimal secondNumber, firstNumber;
+            decimal results = 0;
+
+            while (buttonExit)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Нажмите 1 что бы выйти из калькулятора");
+                string userExit = Console.ReadLine();
+
+                if (Convert.ToInt32(userExit) == 1)
+                {
+                    buttonExit = false;
+                    break;
+                }
+
+                try
+                {
+                    Console.Write("Введите первое число: ");
+                    firstNumber = Convert.ToDecimal(Console.ReadLine());
+
+                    Console.Write("Введите второе число: ");
+                    secondNumber = Convert.ToDecimal(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Такого числа не существует");
+                    break;
+                }
+
+                Console.Write("Введите операцию: ");
+                string userOper = Console.ReadLine();
+
+                switch (userOper)
+                {
+                    case "+":
+                        results = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        results = firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        results = firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        results = firstNumber / secondNumber;
+                        break;
+                    case "%":
+                        results = (firstNumber * secondNumber) / 100;
+                        break;
+                    default:
+                        Console.WriteLine("Я не знаю такой операции!");
+                        break;
+
+                }
+                Console.WriteLine($"Ответ: {results}");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
