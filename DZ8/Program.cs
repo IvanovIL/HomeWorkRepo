c63ceca28687510acd97914983d3ee57d7788f74
Stefan Flyagin <ila723483@gmail.com>
2024-07-25 16:45:50 +0300

    Abstract class animal

---
+++ DZ8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace DZ8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Назовите собаку: ");
+            Dog dog = new Dog();
+
+            dog.SetName(Console.ReadLine());
+            dog.GetName();
+            dog.Eat();
+        }
+        
+        abstract class Animal
+        {
+            string Name = string.Empty;
+            public string SetName(string name)
+            {
+                Name = name;
+                return this.Name;
+            }
+            public void GetName()
+            {
+                
+                Console.WriteLine("Имя собаки " + Name);  
+            }
+
+            public abstract void Eat();
+
+        }
+
+        class Dog : Animal
+        {
+           
+            public Dog()
+            {
+               
+                Animal animal;
+            }
+
+            public override void Eat()
+            {
+                Console.WriteLine("Собака ест!");
+            }
+            
+
+            
+
+        }
+    }
+
+}
+
