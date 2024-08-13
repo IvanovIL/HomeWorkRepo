2d768c588b437f3b64f3a36344b618d0453fc78f
Stefan Flyagin <ila723483@gmail.com>
2024-08-13 12:01:40 +0300

    dz12

---
+++ Dz12 login and password/WrongPasswordException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz12_login_and_password
+{
+    internal class WrongPasswordException : Exception
+    {
+        private string message;
+
+        public WrongPasswordException(string message2) : base(message2)
+        {
+            message2 = message;
+        }
+    }
+}
