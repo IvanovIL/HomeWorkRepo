eb2721dde2aaa9927377f46dcd6f2b728a6e1669
Stefan Flyagin <ila723483@gmail.com>
2024-08-13 11:37:03 +0300

    DZ12

---
+++ DZ12/WrongLoginException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static DZ12.Program;
+
+namespace DZ12
+{
+    internal class WrongLoginException : Exception
+    {
+        private string message;
+
+        public WrongLoginException(string message2) : base(message2)
+        {
+            message2 = message;
+        }
+
+    }
+}
+    
